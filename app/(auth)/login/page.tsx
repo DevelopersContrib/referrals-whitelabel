f1ab1199de34b8491f444f978d2d17182e27df3a
jaysoncano <cano.jayson10@gmail.com>
2024-04-23 14:13:19 +0800

    Feat: create login and register page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,18 +1,37 @@
 import React from "react";
+import { SlLogin } from "react-icons/sl";
 
 const Login = () => {
   return (
     <>
-      <main className="tw-min-h-[calc(100vh-76px-56px)] py-8">
-        <section>
-          <div className="container">
-            <div className="row tw-justify-center">
-              <div className="col-lg-5 tw-border tw-border-[#ddd] tw-flex">
-                <h1>hello world</h1>
+      <main className="tw-min-h-[calc(100vh-76px-56px)] py-8 tw-w-full tw-flex tw-justify-center tw-items-center">
+        <div className="container">
+          <div className="row tw-w-full tw-justify-center tw-items-center">
+            <div className="col-lg-4 tw-flex tw-flex-col">
+              <h1 className="tw-text-3xl tw-font-bold tw-mb-4 tw-text-center">
+                Log in to your account
+              </h1>
+              <div className="tw-bg-[#eee] tw-p-8">
+                <div className="tw-mb-4">
+                  <label htmlFor="">Username</label>
+                  <input type="text" className="form-control" />
+                </div>
+                <div className="tw-mb-4">
+                  <label htmlFor="">Password</label>
+                  <input type="password" className="form-control" />
+                </div>
+                <div className="d-grid">
+                  <button className="btn btn-primary !tw-flex tw-items-center tw-justify-center tw-text-center tw-w-full tw-space-x-2">
+                    <span>
+                      <SlLogin className="tw-w-4 tw-h-4" />
+                    </span>
+                    <span>Log in</span>
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </section>
+        </div>
       </main>
     </>
   );
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { FaUserEdit } from "react-icons/fa";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <>
+      <>
+        <main className="tw-min-h-[calc(100vh-76px-56px)] py-8 tw-w-full tw-flex tw-justify-center tw-items-center">
+          <div className="container">
+            <div className="row tw-w-full tw-justify-center tw-items-center">
+              <div className="col-lg-4 tw-flex tw-flex-col">
+                <h1 className="tw-text-3xl tw-font-bold tw-mb-4 tw-text-center">
+                  Register for an account
+                </h1>
+                <div className="tw-bg-[#eee] tw-p-8">
+                  <div className="tw-mb-4">
+                    <label htmlFor="">Username</label>
+                    <input type="text" className="form-control" />
+                  </div>
+                  <div className="tw-mb-4">
+                    <label htmlFor="">Password</label>
+                    <input type="password" className="form-control" />
+                  </div>
+                  <div className="tw-mb-4">
+                    <label htmlFor="">Confirm Password</label>
+                    <input type="password" className="form-control" />
+                  </div>
+                  <div className="d-grid">
+                    <button className="btn btn-primary !tw-flex tw-items-center tw-justify-center tw-text-center tw-w-full tw-space-x-2">
+                      <span>
+                        <FaUserEdit className="tw-w-4 tw-h-4" />
+                      </span>
+                      <span>Register</span>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </>
+    </>
+  );
 };
 
 export default Register;
