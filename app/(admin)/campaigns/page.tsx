dbbc4dfba98261eb0e354c2cd016d94b96ddf9dd
jaysoncano <jayson@simpleapps.com>
2024-05-01 10:14:37 +0800

    Fix: update sidebard, dashboard and create page for campaigns

---
+++ app/(admin)/campaigns/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+      <div className="flex items-center">
+        <h1 className="text-lg font-semibold md:text-2xl capitalize">
+          Campaigns
+        </h1>
+      </div>
+    </main>
+  );
+};
+
+export default page;
