19ac910a34295ac9af09da724b771f806d57c32b
jaysoncano <jayson@simpleapps.com>
2024-05-02 13:59:27 +0800

    Fix: settings responsive content

--- app/(admin)/settings/page.tsx
+++ app/(admin)/settings/page.tsx
@@ -27,116 +27,120 @@ const Settings = () => {
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
       <div className="">
-        <Tabs defaultValue="account" className="w-1/2">
-          <TabsList className="grid w-full grid-cols-4">
-            <TabsTrigger value="account">Account</TabsTrigger>
-            <TabsTrigger value="password">Password</TabsTrigger>
-            <TabsTrigger value="walletAddress">Wallet Address</TabsTrigger>
-            <TabsTrigger value="deleteAccount">Delete Account</TabsTrigger>
-          </TabsList>
-          <TabsContent value="account">
-            <Card>
-              <CardHeader>
-                <CardTitle>Account</CardTitle>
-                <CardDescription>
-                  Make changes to your account here. Click save when you&apos;re
-                  done.
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="space-y-2">
-                <div className="space-y-1">
-                  <Label htmlFor="name">Full Name</Label>
-                  <Input id="name" defaultValue="John Doe" />
-                </div>
-                <div className="space-y-1">
-                  <Label htmlFor="username">Email Address</Label>
-                  <Input id="username" defaultValue="johndoe@example.com" />
-                </div>
-              </CardContent>
-              <CardFooter>
-                <Button>Save changes</Button>
-              </CardFooter>
-            </Card>
-          </TabsContent>
-          <TabsContent value="password">
-            <Card>
-              <CardHeader>
-                <CardTitle>Password</CardTitle>
-                <CardDescription>
-                  Change your password here. After saving, you&apos;ll be logged
-                  out.
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="space-y-2">
-                <div className="space-y-1">
-                  <Label htmlFor="current">Current password</Label>
-                  <Input id="current" type="password" />
-                </div>
-                <div className="space-y-1">
-                  <Label htmlFor="new">New password</Label>
-                  <Input id="new" type="password" />
-                </div>
-              </CardContent>
-              <CardFooter>
-                <Button>Save password</Button>
-              </CardFooter>
-            </Card>
-          </TabsContent>
-          <TabsContent value="walletAddress">
-            <Card>
-              <CardHeader>
-                <CardTitle>Wallet Address</CardTitle>
-                <CardDescription>
-                  Change your wallet address here.
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="space-y-2">
-                <div className="space-y-1">
-                  <Label htmlFor="current">Wallet Address</Label>
-                  <Input id="walletAddress" type="text" />
-                </div>
-              </CardContent>
-              <CardFooter>
-                <Button>Save Changes</Button>
-              </CardFooter>
-            </Card>
-          </TabsContent>
-          <TabsContent value="deleteAccount">
-            <Card>
-              <CardHeader>
-                <CardTitle>Delete Account</CardTitle>
-                <CardDescription>
-                  Click here to delete your contrib.com Referral Program user
-                  account. Your subscription will be canceled automatically.
-                </CardDescription>
-              </CardHeader>
-              <CardFooter>
-                <AlertDialog>
-                  <AlertDialogTrigger asChild>
-                    <Button variant={"destructive"}>Delete Account</Button>
-                  </AlertDialogTrigger>
-                  <AlertDialogContent>
-                    <AlertDialogHeader>
-                      <AlertDialogTitle>
-                        Are you absolutely sure?
-                      </AlertDialogTitle>
-                      <AlertDialogDescription>
-                        This action cannot be undone. This will permanently
-                        delete your account and remove your data from our
-                        servers.
-                      </AlertDialogDescription>
-                    </AlertDialogHeader>
-                    <AlertDialogFooter>
-                      <AlertDialogCancel className="mt-0">
-                        Cancel
-                      </AlertDialogCancel>
-                      <AlertDialogAction>Continue</AlertDialogAction>
-                    </AlertDialogFooter>
-                  </AlertDialogContent>
-                </AlertDialog>
-              </CardFooter>
-            </Card>
-          </TabsContent>
+        <Tabs defaultValue="account">
+          <div className="inline-flex">
+            <TabsList className="inline-flex w-full">
+              <TabsTrigger value="account">Account</TabsTrigger>
+              <TabsTrigger value="password">Password</TabsTrigger>
+              <TabsTrigger value="walletAddress">Wallet Address</TabsTrigger>
+              <TabsTrigger value="deleteAccount">Delete Account</TabsTrigger>
+            </TabsList>
+          </div>
+          <div className="w-full lg:w-1/2">
+            <TabsContent value="account">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Account</CardTitle>
+                  <CardDescription>
+                    Make changes to your account here. Click save when
+                    you&apos;re done.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-2">
+                  <div className="space-y-1">
+                    <Label htmlFor="name">Full Name</Label>
+                    <Input id="name" defaultValue="John Doe" />
+                  </div>
+                  <div className="space-y-1">
+                    <Label htmlFor="username">Email Address</Label>
+                    <Input id="username" defaultValue="johndoe@example.com" />
+                  </div>
+                </CardContent>
+                <CardFooter>
+                  <Button>Save changes</Button>
+                </CardFooter>
+              </Card>
+            </TabsContent>
+            <TabsContent value="password">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Password</CardTitle>
+                  <CardDescription>
+                    Change your password here. After saving, you&apos;ll be
+                    logged out.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-2">
+                  <div className="space-y-1">
+                    <Label htmlFor="current">Current password</Label>
+                    <Input id="current" type="password" />
+                  </div>
+                  <div className="space-y-1">
+                    <Label htmlFor="new">New password</Label>
+                    <Input id="new" type="password" />
+                  </div>
+                </CardContent>
+                <CardFooter>
+                  <Button>Save password</Button>
+                </CardFooter>
+              </Card>
+            </TabsContent>
+            <TabsContent value="walletAddress">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Wallet Address</CardTitle>
+                  <CardDescription>
+                    Change your wallet address here.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-2">
+                  <div className="space-y-1">
+                    <Label htmlFor="current">Wallet Address</Label>
+                    <Input id="walletAddress" type="text" />
+                  </div>
+                </CardContent>
+                <CardFooter>
+                  <Button>Save Changes</Button>
+                </CardFooter>
+              </Card>
+            </TabsContent>
+            <TabsContent value="deleteAccount">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Delete Account</CardTitle>
+                  <CardDescription>
+                    Click here to delete your contrib.com Referral Program user
+                    account. Your subscription will be canceled automatically.
+                  </CardDescription>
+                </CardHeader>
+                <CardFooter>
+                  <AlertDialog>
+                    <AlertDialogTrigger asChild>
+                      <Button variant={"destructive"}>Delete Account</Button>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                      <AlertDialogHeader>
+                        <AlertDialogTitle>
+                          Are you absolutely sure?
+                        </AlertDialogTitle>
+                        <AlertDialogDescription>
+                          This action cannot be undone. This will permanently
+                          delete your account and remove your data from our
+                          servers.
+                        </AlertDialogDescription>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter>
+                        <AlertDialogCancel className="mt-0">
+                          Cancel
+                        </AlertDialogCancel>
+                        <AlertDialogAction>Continue</AlertDialogAction>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
+                </CardFooter>
+              </Card>
+            </TabsContent>
+          </div>
         </Tabs>
       </div>
     </main>
