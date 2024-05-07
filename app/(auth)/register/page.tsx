36b38b540b6ffb8d5b79956deae7cf0d34f767e9
jaysoncano <jayson@simpleapps.com>
2024-05-07 13:43:31 +0800

    Fix: update registration

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
-import React from "react";
-
-
+import { FaUserCheck } from "react-icons/fa6";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import {
@@ -22,8 +20,7 @@ export default function Register() {
     userPassword: "",
     userPasswordConfirm: "",
     domain: "whitelabel.referrals.com"
-  }
-
+  };
 
   const initialErrors = {
     validate: false,
@@ -31,77 +28,108 @@ export default function Register() {
     nameError: "",
     passwordError: "",
     cpasswordError: ""
-  }
+  };
 
   const [data, setData] = useState(initialValues);
   const [errors, setErrors] = useState(initialErrors);
   const [success, setSuccess] = useState(false);
+  const [isSubmit, setIsSubmit] = useState(false);
+  const [counter, setCounter] = useState<number>(5);
 
   useEffect(() => {
     const validateErrors = () => {
       let dataErrors;
       dataErrors = {
         validate: false, // Include validate property
-        emailError: (data.userEmail ? "" : "Email is required") ||
-        (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(data.userEmail)
-          ? ""
-          : "Invalid Email"),
+        emailError:
+          (data.userEmail ? "" : "Email is required") ||
+          (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(data.userEmail)
+            ? ""
+            : "Invalid Email"),
         nameError: data.userName ? "" : "Name is required.",
         passwordError: data.userPassword ? "" : "Password is required.",
         cpasswordError:
-            (data.userPasswordConfirm ? "" : "Confirm password is required.") ||
-            (data.userPassword !== data.userPasswordConfirm
-              ? "Confirm password did not match."
-              : ""),
-      }
+          (data.userPasswordConfirm ? "" : "Confirm password is required.") ||
+          (data.userPassword !== data.userPasswordConfirm
+            ? "Confirm password did not match."
+            : "")
+      };
       setErrors(dataErrors);
     };
     validateErrors();
-  },[data]);
+  }, [data]);
 
-  const handleChange = (e:any) => {
+  const handleChange = (e: any) => {
     setData({ ...data, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = async (event:any) => {
+  const handleSubmit = async (event: any) => {
+    setIsSubmit(true);
     event.preventDefault();
     const isValid = !Object.values(errors).some((v) => v);
-    if(isValid) {
+    if (isValid) {
       const response = await fetch("/api/register", {
         method: "POST",
         body: JSON.stringify(data)
       })
         .then((response) => {
+          setIsSubmit(false);
           // Handle response here
           setSuccess(true);
+          const counterInterval = setInterval(() => {
+            setCounter((prev) => {
+              if (prev === 1) {
+                clearInterval(counterInterval);
+                window.location.href = "/login";
+              }
+
+              return prev - 1;
+            });
+          }, 1000);
+
+          return () => clearInterval(counterInterval);
         })
         .catch((error) => {
+          setIsSubmit(false);
           // Handle error here
         });
-    }else{
+    } else {
       setErrors({ ...errors, ["validate"]: true });
+      setIsSubmit(false);
     }
-  }
+  };
 
   return (
     <>
       <main className="min-h-[calc(100vh-56px-32px)] py-8 w-full flex justify-center items-center">
         <Card className="mx-auto max-w-sm">
-          <CardHeader>
-            <CardTitle className="text-xl">Sign Up</CardTitle>
-            <CardDescription>
-              Enter your information to create an account
-            </CardDescription>
-          </CardHeader>
-          {
-            !success ? (
-              <>
-                <CardContent>
+          {!success ? (
+            <>
+              <CardHeader>
+                <CardTitle className="text-xl">Sign Up</CardTitle>
+                <CardDescription>
+                  Enter your information to create an account
+                </CardDescription>
+              </CardHeader>
+
+              <CardContent>
+                <form>
                   <div className="grid gap-4">
                     <div className="grid gap-2">
-                        <Label htmlFor="first-name">First name</Label>
-                        <Input id="first-name" name="userName" onChange={handleChange} value={data.userName} placeholder="First name" required />
-                        {errors.validate ? <div className="d-block text-danger small mt-2">{errors.nameError}</div> : null}
+                      <Label htmlFor="first-name">First name</Label>
+                      <Input
+                        id="first-name"
+                        name="userName"
+                        onChange={handleChange}
+                        value={data.userName}
+                        placeholder="First name"
+                        required
+                      />
+                      {errors.validate ? (
+                        <div className="d-block text-danger small">
+                          * {errors.nameError}
+                        </div>
+                      ) : null}
                     </div>
                     <div className="grid gap-2">
                       <Label htmlFor="email">Email</Label>
@@ -114,33 +142,92 @@ export default function Register() {
                         placeholder="Email address"
                         required
                       />
-                      {errors.validate ? <div className="d-block text-danger small mt-2">{errors.emailError}</div> : null}
+                      {errors.validate ? (
+                        <div className="d-block text-danger small">
+                          * {errors.emailError}
+                        </div>
+                      ) : null}
                     </div>
                     <div className="grid gap-2">
                       <Label htmlFor="password">Password</Label>
-                      <Input id="password" name="userPassword" type="password" placeholder="Password" onChange={handleChange} value={data.userPassword} />
-                      {errors.validate ? <div className="d-block text-danger small mt-2">{errors.passwordError}</div> : null}
+                      <Input
+                        id="password"
+                        name="userPassword"
+                        type="password"
+                        placeholder="Password"
+                        onChange={handleChange}
+                        value={data.userPassword}
+                        autoComplete="off"
+                      />
+                      {errors.validate ? (
+                        <div className="d-block text-danger small">
+                          * {errors.passwordError}
+                        </div>
+                      ) : null}
                     </div>
                     <div className="grid gap-2">
                       <Label htmlFor="password">Confirm Password</Label>
-                      <Input id="userPasswordConfirm" type="password" name="userPasswordConfirm" placeholder="Password" onChange={handleChange} value={data.userPasswordConfirm} />
-                      {errors.validate ? <div className="d-block text-danger small mt-2">{errors.cpasswordError}</div> : null}
+                      <Input
+                        id="userPasswordConfirm"
+                        type="password"
+                        name="userPasswordConfirm"
+                        placeholder="Password"
+                        onChange={handleChange}
+                        value={data.userPasswordConfirm}
+                        autoComplete="off"
+                      />
+                      {errors.validate ? (
+                        <div className="d-block text-danger small">
+                          * {errors.cpasswordError}
+                        </div>
+                      ) : null}
                     </div>
-                    <Button className="w-full" onClick={handleSubmit}>
-                      Create an account
+                    <Button
+                      className="w-full"
+                      disabled={isSubmit}
+                      onClick={handleSubmit}
+                    >
+                      {isSubmit ? "Creating Account..." : "Create an account"}
                     </Button>
                   </div>
-                  <div className="mt-4 text-center text-sm">
-                    Already have an account?{" "}
-                    <Link href="#" className="underline">
+                </form>
+                <div className="mt-4 text-center text-sm">
+                  Already have an account?{" "}
+                  <Link href="/login" className="underline">
+                    Log in
+                  </Link>
+                </div>
+              </CardContent>
+            </>
+          ) : null}
+          {success ? (
+            <>
+              <CardHeader>
+                <CardTitle className="text-xl text-primary flex items-center">
+                  <span className="mr-2">
+                    <FaUserCheck />
+                  </span>{" "}
+                  <span>Success!</span>
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="text-black">
+                  <p>
+                    Congratulations on signing up! Get ready for a journey
+                    towards success!
+                  </p>
+                  <br />
+                  <p className="text-sm">
+                    You will be redirected to{" "}
+                    <Link href="/login" className="underline">
                       Log in
-                    </Link>
-                  </div>
-                </CardContent>
-              </>
-            ):null
-          }
-          {success ? <div className="d-block text-success mt-2">Registration Successful</div> : null}     
+                    </Link>{" "}
+                    in about {counter} secs...
+                  </p>
+                </div>
+              </CardContent>
+            </>
+          ) : null}
         </Card>
       </main>
     </>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,22 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.scss";
-import { Getwlsettings} from "../data/data";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Getwlsettings } from "../data/data";
 
 export async function generateMetadata() {
   const c = await Getwlsettings();
-  console.log(c.data)
+  console.log(c.data);
   return {
-		title: c.data.meta_title,
-		description: c.data.meta_desc,
-    keywords: c.data.meta_keywords,
-   
-	}
-};
-
-
+    title: c.data.meta_title,
+    description: c.data.meta_desc,
+    keywords: c.data.meta_keywords
+  };
+}
 
 export default async function RootLayout({
-  children,
+  children
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   const c = await Getwlsettings();
   return (
@@ -32,20 +26,14 @@ export default async function RootLayout({
           <script dangerouslySetInnerHTML={{ __html: c.header_script }} />
         )}
       </head>
-      <body className={inter.className}>
-        <header>
-          {c.header_script ? null : c.header_script}
-        </header>
+      <body>
+        <header>{c.header_script ? null : c.header_script}</header>
         {children}
-       
-          {c.footer_script && c.footer_script !== '' && (
-            <script dangerouslySetInnerHTML={{ __html: c.footer_script }} />
-          )}
-      
+
+        {c.footer_script && c.footer_script !== "" && (
+          <script dangerouslySetInnerHTML={{ __html: c.footer_script }} />
+        )}
       </body>
     </html>
   );
 }
-
-
-
