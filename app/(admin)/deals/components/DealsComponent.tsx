34076f8aabb0e5cd2ba89e4d48f3867c0a0f487c
jaysoncano <jayson@simpleapps.com>
2024-05-14 10:05:19 +0800

    Fix: deals component

--- app/(admin)/deals/components/DealsComponent.tsx
+++ app/(admin)/deals/components/DealsComponent.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 import { IoDiamondSharp } from "react-icons/io5";
 import { IoIosSearch } from "react-icons/io";
+import { CiBadgeDollar } from "react-icons/ci";
 import { Checkbox } from "@/components/ui/checkbox";
 import { GetDeals } from "../../../../data/data";
-
 
 export default async function DealsComponent() {
   const deals = await GetDeals();
@@ -14,59 +14,68 @@ export default async function DealsComponent() {
     <>
       <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
         <div className="grid grid-cols-1 md:grid-cols-9 md:gap-4 gap-y-4">
-          <div className="col-span-7 grid grid-cols-1 lg:grid-cols-3 gap-4">
-          {Array.isArray(dealsData) && dealsData.length > 0 ? (
-          dealsData.map((deals) => (
-            <a
-              href={deals.url}
-              className="border border-solid border-[hsl(0_0%_0%/0.05)!important] p-4 rounded-xl flex flex-col gap-y-4 hover:translate-y-[-5px] hover:transition-all hover:duration-300 hover:scale-[1.02] hover:shadow-[rgba(149,157,165,0.2)_0px_8px_24px] transition-all duration-200"
-              key={deals.id}
-            >
-              <div className="text-right">
-                <div className="inline-flex bg-[#ffd50033] items-center justify-center p-[4px_7px] gap-x-2 rounded-lg text-md">
-                  <div>
-                    <IoDiamondSharp />
-                  </div>
-                  <div>Featured</div>
-                </div>
-              </div>
-              <div>
-                <Image
-                  src={deals.banner}
-                  width={0}
-                  height={0}
-                  alt=""
-                  className="img-fluid h-[70px] object-contain w-full"
-                  sizes="100vw"
-                />
-              </div>
-              <div className="bg-[#f2f2ff7a] p-4 rounded-lg flex flex-col gap-y-4">
-                <div className="">
-                  <h3>{deals.title}</h3>
-                </div>
-                <div className="text-sm text-muted">
-                  <p>
-                    {deals.description}
-                  </p>
-                </div>
-                <div>
-                  <div className="rounded-lg py-2 px-4 bg-primary text-white text-center w-full capitalize flex items-center justify-center">
-                    <div className="mr-1">
-                      <IoIosSearch className="h-6 w-6" />
+          <div className="col-span-7 grid grid-cols-1 gap-y-4">
+            {Array.isArray(dealsData) && dealsData.length > 0 ? (
+              dealsData.map((deals) => (
+                <a
+                  href={deals.url}
+                  className="border border-solid border-[hsl(0_0%_0%/0.05)!important] p-4 rounded-xl flex flex-col gap-y-4 hover:translate-y-[-5px] hover:transition-all hover:duration-300 hover:scale-[1.02] hover:shadow-[rgba(149,157,165,0.2)_0px_8px_24px] transition-all duration-200"
+                  key={deals.id}
+                >
+                  <div className="w-full grid grid-cols-1 gap-y-4 lg:grid-cols-8 lg:gap-x-4">
+                    <div className="col-span-2 flex w-full">
+                      <div className="bg-[#f2f2ff7a] p-4 rounded-lg w-full flex items-center justify-center">
+                        <Image
+                          src={deals.banner}
+                          width={0}
+                          height={0}
+                          alt=""
+                          className="img-fluid h-auto max-h-[110px] object-contain w-full"
+                          sizes="100vw"
+                        />
+                      </div>
                     </div>
-                    <div>view deal</div>
+                    <div className="col-span-6 flex flex-col gap-y-4">
+                      <div className="justify-end flex items-center gap-x-2">
+                        <div className="inline-flex bg-[#ffd50033] items-center justify-center p-[4px_7px] gap-x-2 rounded-lg text-md">
+                          <div>
+                            <IoDiamondSharp />
+                          </div>
+                          <div className="capitalize">category</div>
+                        </div>
+                        <div className="inline-flex text-white bg-[#FF6067] items-center justify-center p-[4px_7px] gap-x-2 rounded-lg text-md">
+                          <div>
+                            <CiBadgeDollar className="h-6 w-6" />
+                          </div>
+                          <div className="capitalize">19.00</div>
+                        </div>
+                      </div>
+
+                      <div className=" flex flex-col gap-y-4">
+                        <div className="">
+                          <h3>{deals.title}</h3>
+                        </div>
+                        <div className="text-sm text-black/60">
+                          <p>{deals.description}</p>
+                        </div>
+                        <div>
+                          <div className="rounded-lg py-2 px-4 bg-primary text-white text-center w-full capitalize flex items-center justify-center">
+                            <div className="mr-1">
+                              <IoIosSearch className="h-6 w-6" />
+                            </div>
+                            <div>view deal</div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-            </a>
-          ))
-        ) : (
-            <div className="col-span-4 w-full flex items-center justify-center min-h-[30vh]">
-              <div className="text-3xl text-black/30">
-                No Deals available
-              </div>
-            </div> // Provide a fallback if campaignData is empty or not an array
-          )}
+                </a>
+              ))
+            ) : (
+              <div className="col-span-8 w-full flex items-center justify-center min-h-[30vh]">
+                <div className="text-3xl text-black/30">No Deals available</div>
+              </div> // Provide a fallback if campaignData is empty or not an array
+            )}
           </div>
           <div className="col-span-2">
             <div className="border border-solid border-[hsl(0_0%_0%/0.05)!important] rounded-lg w-full">
@@ -158,5 +167,4 @@ export default async function DealsComponent() {
       </main>
     </>
   );
-};
-
+}
