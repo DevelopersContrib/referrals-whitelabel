fc17e651e1cc749be9f27e5255de80b2896945ac
jaysoncano <jayson@simpleapps.com>
2024-05-15 13:53:21 +0800

    Feat: about page

--- app/(public)/about/page.tsx
+++ app/(public)/about/page.tsx
@@ -1,7 +1,239 @@
+import Image from "next/image";
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <main className="min-h-[calc(100vh-56px-48px)]">
+      <section className="py-4 bg-white text-black">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <h1 className="capitalize font-bold text-2xl mb-4">ABOUT US</h1>
+              <p className="mb-4">
+                Referrals.com is in the alpha stage, and we're working hard
+                behind the scenes to build the ultimate referrals platform.
+                Thanks for taking the time to check us out and stay tuned for
+                more robust features to help you generate business. We're part
+                of the Referral Ventures network.
+              </p>
+              <h1 className="capitalize font-bold text-2xl mb-4">TEAM</h1>
+            </div>
+            <div className="col-lg-12">
+              <div className="grid w-full grid-cols-1 lg:grid-cols-4 gap-y-4 lg:gap-4">
+                <div className="bg-[#ff606717] p-4 w-full flex-col text-center justify-center items-center gap-y-4">
+                  <div className="flex items-center justify-center mb-4">
+                    <Image
+                      alt=""
+                      src={
+                        "https://manage.vnoc.com/uploads/picture/image1651198240_1"
+                      }
+                      sizes="100vw"
+                      width={0}
+                      height={0}
+                      className="img-fluid w-[225px] h-[225px] object-cover rounded-full"
+                      loading="lazy"
+                    />
+                  </div>
+                  <div className="text-center">
+                    <h4 className="font-bold capitalize mb-1 text-2xl">
+                      kareen castanos
+                    </h4>
+                    <p className="capitalize text-sm text-[#FF6067]">
+                      web developer
+                    </p>
+                  </div>
+                </div>
+                <div className="bg-[#ff606717] p-4 w-full flex-col text-center justify-center items-center gap-y-4">
+                  <div className="flex items-center justify-center mb-4">
+                    <Image
+                      alt=""
+                      src={
+                        "https://manage.vnoc.com/uploads/picture/image1651198372_5"
+                      }
+                      sizes="100vw"
+                      width={0}
+                      height={0}
+                      className="img-fluid w-[225px] h-[225px] object-cover rounded-full"
+                      loading="lazy"
+                    />
+                  </div>
+                  <div className="text-center">
+                    <h4 className="font-bold capitalize mb-1 text-2xl">
+                      Catherine Hortillosa
+                    </h4>
+                    <p className="capitalize text-sm text-[#FF6067]">
+                      Content Manager
+                    </p>
+                  </div>
+                </div>
+                <div className="bg-[#ff606717] p-4 w-full flex-col text-center justify-center items-center gap-y-4">
+                  <div className="flex items-center justify-center mb-4">
+                    <Image
+                      alt=""
+                      src={
+                        "https://manage.vnoc.com/uploads/picture/image1376285817_global-273x300.jpg"
+                      }
+                      sizes="100vw"
+                      width={0}
+                      height={0}
+                      className="img-fluid w-[225px] h-[225px] object-cover rounded-full"
+                      loading="lazy"
+                    />
+                  </div>
+                  <div className="text-center">
+                    <h4 className="font-bold capitalize mb-1 text-2xl">
+                      Asset Manager
+                    </h4>
+                    <p className="capitalize text-sm text-[#FF6067]">
+                      Execution Officer
+                    </p>
+                  </div>
+                </div>
+                <div className="bg-[#ff606717] p-4 w-full flex-col text-center justify-center items-center gap-y-4">
+                  <div className="flex items-center justify-center mb-4">
+                    <Image
+                      alt=""
+                      src={
+                        "https://manage.vnoc.com/uploads/picture/image1649647693_12"
+                      }
+                      sizes="100vw"
+                      width={0}
+                      height={0}
+                      className="img-fluid w-[225px] h-[225px] object-cover rounded-full"
+                      loading="lazy"
+                    />
+                  </div>
+                  <div className="text-center">
+                    <h4 className="font-bold capitalize mb-1 text-2xl">
+                      Maida Barrientos
+                    </h4>
+                    <p className="capitalize text-sm text-[#FF6067]">
+                      Project Manager
+                    </p>
+                  </div>
+                </div>
+                <div className="bg-[#ff606717] p-4 w-full flex-col text-center justify-center items-center gap-y-4">
+                  <div className="flex items-center justify-center mb-4">
+                    <Image
+                      alt=""
+                      src={
+                        "https://manage.vnoc.com/uploads/picture/image1651198017_39"
+                      }
+                      sizes="100vw"
+                      width={0}
+                      height={0}
+                      className="img-fluid w-[225px] h-[225px] object-cover rounded-full"
+                      loading="lazy"
+                    />
+                  </div>
+                  <div className="text-center">
+                    <h4 className="font-bold capitalize mb-1 text-2xl">
+                      Jayson Cano
+                    </h4>
+                    <p className="capitalize text-sm text-[#FF6067]">
+                      frontend developer
+                    </p>
+                  </div>
+                </div>
+                <div className="bg-[#ff606717] p-4 w-full flex-col text-center justify-center items-center gap-y-4">
+                  <div className="flex items-center justify-center mb-4">
+                    <Image
+                      alt=""
+                      src={
+                        "https://manage.vnoc.com/uploads/picture/image1649828116_41"
+                      }
+                      sizes="100vw"
+                      width={0}
+                      height={0}
+                      className="img-fluid w-[225px] h-[225px] object-cover rounded-full"
+                      loading="lazy"
+                    />
+                  </div>
+                  <div className="text-center">
+                    <h4 className="font-bold capitalize mb-1 text-2xl">
+                      Ronan Lee
+                    </h4>
+                    <p className="capitalize text-sm text-[#FF6067]">
+                      web developer
+                    </p>
+                  </div>
+                </div>
+                <div className="bg-[#ff606717] p-4 w-full flex-col text-center justify-center items-center gap-y-4">
+                  <div className="flex items-center justify-center mb-4">
+                    <Image
+                      alt=""
+                      src={
+                        "https://manage.vnoc.com/uploads/picture/image1651193620_51"
+                      }
+                      sizes="100vw"
+                      width={0}
+                      height={0}
+                      className="img-fluid w-[225px] h-[225px] object-cover rounded-full"
+                      loading="lazy"
+                    />
+                  </div>
+                  <div className="text-center">
+                    <h4 className="font-bold capitalize mb-1 text-2xl">
+                      Andre Cipriano
+                    </h4>
+                    <p className="capitalize text-sm text-[#FF6067]">
+                      web designer
+                    </p>
+                  </div>
+                </div>
+                <div className="bg-[#ff606717] p-4 w-full flex-col text-center justify-center items-center gap-y-4">
+                  <div className="flex items-center justify-center mb-4">
+                    <Image
+                      alt=""
+                      src={
+                        "https://manage.vnoc.com/uploads/picture/image1691365132_115"
+                      }
+                      sizes="100vw"
+                      width={0}
+                      height={0}
+                      className="img-fluid w-[225px] h-[225px] object-cover rounded-full"
+                      loading="lazy"
+                    />
+                  </div>
+                  <div className="text-center">
+                    <h4 className="font-bold capitalize mb-1 text-2xl">
+                      Lucille Conde
+                    </h4>
+                    <p className="capitalize text-sm text-[#FF6067]">
+                      social and media manager
+                    </p>
+                  </div>
+                </div>
+                <div className="bg-[#ff606717] p-4 w-full flex-col text-center justify-center items-center gap-y-4">
+                  <div className="flex items-center justify-center mb-4">
+                    <Image
+                      alt=""
+                      src={
+                        "https://manage.vnoc.com/uploads/picture/image1687480235_806"
+                      }
+                      sizes="100vw"
+                      width={0}
+                      height={0}
+                      className="img-fluid w-[225px] h-[225px] object-cover rounded-full"
+                      loading="lazy"
+                    />
+                  </div>
+                  <div className="text-center">
+                    <h4 className="font-bold capitalize mb-1 text-2xl">
+                      Joseph Javier
+                    </h4>
+                    <p className="capitalize text-sm text-[#FF6067]">
+                      web developer
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default About;
