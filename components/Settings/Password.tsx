50da28b8e1e1c422d9aa94413a23227e505239b2
Miexie R2 Network <sephjavier@gmail.com>
2024-05-22 10:46:43 +0800

    dynamic password submission

--- components/Settings/Password.tsx
+++ components/Settings/Password.tsx
@@ -19,34 +19,123 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { FaCheck } from "react-icons/fa6";
 
-export default function Password() {
-    const { data: session } = useSession();
-    const [error, setError] = useState("");
-    const [updating, setUpdating] = useState(false);
-    if (session) {
-        return (
-            <Card>
-                <CardHeader>
-                    <CardTitle>Password</CardTitle>
-                    <CardDescription>
-                    Change your password here. After saving, you&apos;ll be
-                    logged out.
-                    </CardDescription>
-                </CardHeader>
-                <CardContent className="space-y-2">
-                    <div className="space-y-1">
-                    <Label htmlFor="current">Current password</Label>
-                    <Input id="current" type="password" />
-                    </div>
-                    <div className="space-y-1">
-                    <Label htmlFor="new">New password</Label>
-                    <Input id="new" type="password" />
-                    </div>
-                </CardContent>
-                <CardFooter>
-                    <Button>Save password</Button>
-                </CardFooter>
-            </Card>
-        );
+export default function Account() {
+  const { data: session } = useSession();
+  const [error, setError] = useState("");
+  const [updating, setUpdating] = useState(false);
+
+  const schema = Yup.object().shape({
+    password: Yup.string().required("Enter your password."),
+    rePassword: Yup.string().required("Please confirm your password."),
+  });
+
+  const handleClick = async (values: any) => {
+    setError((prev) => "");
+    setUpdating(true);
+
+    const updated = await fetch("/api/user/updatepassword", {
+      method: "POST",
+      body: JSON.stringify({
+        password: values.password,
+      })
+    });
+
+    const result = await updated.json();
+
+    console.log(result);
+    if (!result.success) {
+      setError(result.error);
+      setUpdating(false);
+    } else {
+      Swal.fire({
+        title: "Success!",
+        text: "You successfully updated your details!",
+        icon: "success",
+        confirmButtonText: "Close"
+      });
+      setUpdating(false);
     }
+
+    if (error) {
+      Swal.fire({
+        title: "Error!",
+        text: error,
+        icon: "error",
+        confirmButtonText: "Close"
+      });
+      setError("");
+    }
+  };
+
+  const InputPassword = ({ name, ...props }: any) => {
+    return <Input {...props} name={name} type="password" />;
+  };
+
+  const InputRepassword = ({ name, ...props }: any) => {
+    return <Input {...props} name={name} type="password" />;
+  };
+
+  if (session) {
+    return (
+      <>
+        <Card>
+          <CardHeader>
+            <CardTitle>Password</CardTitle>
+            <CardDescription>Change your password here.</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Formik
+              initialValues={{
+                password: '',
+                repassword: ''
+              }}
+              validationSchema={schema}
+              onSubmit={(values: any) => {
+                setTimeout(() => {
+                  handleClick(values);
+                }, 500);
+              }}
+            >
+              {(formik) => (
+                <Form>
+                  <div className="mb-4">
+                    <Label htmlFor="name">Password</Label>
+                    <Field id="password" as={InputPassword} name="password" />
+                    <ErrorMessage
+                      component="small"
+                      className="text-[rgb(220,53,69)]"
+                      name="password"
+                    />
+                  </div>
+
+                  <div className="mb-4">
+                    <Label htmlFor="email">Re Type Password</Label>
+                    <Field id="repassword" as={InputRepassword} name="repassword" />
+                    <ErrorMessage
+                      component="small"
+                      className="text-[rgb(220,53,69)]"
+                      name="rePassword"
+                    />
+                  </div>
+                  <Button type="submit" disabled={updating}>
+                    {updating ? (
+                      <>
+                        <ReloadIcon className="h-4 w-4 mr-1 animate-spin" />
+                        Updating...
+                      </>
+                    ) : (
+                      <>
+                        <FaCheck className="h-4 w-4 mr-1" />
+                        Save Changes
+                      </>
+                    )}
+                  </Button>
+                </Form>
+              )}
+            </Formik>
+          </CardContent>
+        </Card>
+      </>
+    );
+  }
 }
