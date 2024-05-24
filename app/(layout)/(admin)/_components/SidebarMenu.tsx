45d4c081e230cef672d60225785d3993d7a40f2f
jaysoncano <jayson@simpleapps.com>
2024-05-24 09:59:48 +0800

    Fix: dynamic sidebar

--- app/(layout)/(admin)/_components/SidebarMenu.tsx
+++ app/(layout)/(admin)/_components/SidebarMenu.tsx
@@ -17,59 +17,67 @@ import {
 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { usePathname } from "next/navigation"; // Updated hook to use
 
 const SidebarMenu = ({ logo }: { logo: string }) => {
+  const pathname = usePathname();
+  const [currentPath, setCurrentPath] = useState("");
+
+  useEffect(() => {
+    setCurrentPath(pathname);
+  }, [pathname]);
+
+  const menuItems = [
+    {
+      href: "/dashboard",
+      icon: <LineChart className="h-4 w-4" />,
+      label: "Dashboard"
+    },
+    { href: "/deals", icon: <Star className="h-4 w-4" />, label: "Deals" },
+    {
+      href: "/campaigns",
+      icon: <Layers className="h-4 w-4" />,
+      label: "Campaigns"
+    },
+    { href: "/rewards", icon: <Gift className="h-4 w-4" />, label: "Rewards" },
+    { href: "/settings", icon: <Cog className="h-4 w-4" />, label: "Settings" }
+  ];
+
   return (
-    <>
-      <div className="hidden border-r bg-gray-100/50 md:block">
-        <div className="flex h-full max-h-screen flex-col gap-2">
-          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-            <a href="/" className="flex items-center gap-2 font-semibold">
-              <span>
-                <Image
-                  loader={imageLoader}
-                  src={logo}
-                  alt="referral"
-                  width={0}
-                  height={0}
-                  className="w-full max-w-full h-[30px] object-contain"
-                  loading="lazy"
-                />
-              </span>
-            </a>
-            {/* <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
-              <Bell className="h-4 w-4" />
-              <span className="sr-only">Toggle notifications</span>
-            </Button> */}
-          </div>
-          <div className="flex-1">
-            <nav className="grid items-start px-2 text-sm font-medium lg:px-4 sidebar">
-              <a href="/dashboard" className="sidebar-link active">
-                <LineChart className="h-4 w-4" />
-                Dashboard
+    <div className="hidden border-r bg-gray-100/50 md:block">
+      <div className="flex h-full max-h-screen flex-col gap-2">
+        <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+          <a href="/" className="flex items-center gap-2 font-semibold">
+            <span>
+              <Image
+                loader={imageLoader}
+                src={logo}
+                alt="referral"
+                width={0}
+                height={0}
+                className="w-full max-w-full h-[30px] object-contain"
+                loading="lazy"
+              />
+            </span>
+          </a>
+        </div>
+        <div className="flex-1">
+          <nav className="grid items-start px-2 text-sm font-medium lg:px-4 sidebar">
+            {menuItems.map((item) => (
+              <a
+                href={item.href}
+                key={item.href}
+                className={`sidebar-link ${currentPath === item.href ? "active" : ""}`}
+              >
+                {item.icon}
+                {item.label}
               </a>
-              <a href="/deals" className="sidebar-link">
-                <Star className="h-4 w-4" />
-                Deals
-              </a>
-              <a href="/campaigns" className="sidebar-link">
-                <Layers className="h-4 w-4" />
-                Campaigns
-              </a>
-              <a href="/rewards" className="sidebar-link">
-                <Gift className="h-4 w-4" />
-                Rewards
-              </a>
-              <a href="/settings" className="sidebar-link">
-                <Cog className="h-4 w-4" />
-                Settings
-              </a>
-            </nav>
-          </div>
+            ))}
+          </nav>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
