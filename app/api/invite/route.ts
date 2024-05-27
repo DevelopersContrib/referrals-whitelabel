71bc35ceea197b23886c9e84b75efad04393810e
ron <row_none@yahoo.com>
2024-05-26 19:03:28 -0700

    added campart_id

--- app/api/invite/route.ts
+++ app/api/invite/route.ts
@@ -11,15 +11,17 @@ export const POST = async (req: Request) => {
     };
 
   const apiUrl = process.env.API_URL + 'campaigns/join?key=' + process.env.API_KEY+'&domain='+data.domain+'&campaign_id='+data.campaign_id+'&userid='+session?.id;
-  
+
+ 
   const res = await axios.get(apiUrl, config);
-  
   if(res.data.success){
+
     const apiUrl = process.env.API_URL + 'invite/email/?key=' + process.env.API_KEY;
     const params = new URLSearchParams(); 
     params.append('domain', data.domain);
     params.append('email', data.email);
     params.append('name', data.name);
+    params.append('campart_id', res.data.campart_id);
     params.append('token', session?.token!);
 
     const headers = { 'Authorization': 'Bearer '+session?.token }; // auth header with bearer token
