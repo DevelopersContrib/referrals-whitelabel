7cfbf8a0dff47a8826d838493dbe7483df27764a
Miexie R2 Network <sephjavier@gmail.com>
2024-06-17 09:27:59 +0800

    autologin

--- app/(public)/account/campaign/[email]/[id]/page.tsx
+++ app/(public)/account/campaign/[email]/[id]/page.tsx
@@ -1,13 +1,48 @@
 // pages/redirect.js
 "use client"; // This directive marks the file as a client component
+import { useState, useEffect } from "react";
+import { signIn } from "next-auth/react";
+//import { useEffect } from 'react';
 
-import { useEffect } from 'react';
+const RedirectPage = async({
+  params
+}: {
+  params: { email: string, id:string };
+}) => {
+  const decodedEmail = atob(params.email);
+  const initialValues = {
+    userEmail: decodedEmail,
+    userPassword: "",
+    domain: ""
+  };
+  console.log(params.email);
+  console.log(params.id);
+ 
+  console.log(decodedEmail);
+  const [data, setData] = useState(initialValues);
+  const response = await fetch("/api/login", {
+    method: "POST",
+    body: JSON.stringify(data)
+  });
 
-const RedirectPage = () => {
-  useEffect(() => {
+  if (response.ok) {
+   
+    console.log(response);
+    await signIn("credentials", {
+      redirect: false,
+      email: data.userEmail,
+      password: data.userPassword,
+      domain: data.domain
+    });
+    window.location.replace("/dashboard");
+  } else {
+    console.log("login error");
+   
+  }
+ /* useEffect(() => {
     // Redirect to the homepage
     window.location.href = '/';
-  }, []);
+  }, []);*/
 
  
 };
