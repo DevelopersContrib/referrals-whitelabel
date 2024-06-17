a6756d25a018b1d1178b1c023d3fe7008d03e8dc
ron <row_none@yahoo.com>
2024-06-16 19:22:21 -0700

    test decode

--- app/(public)/account/campaign/[email]/[id]/page.tsx
+++ app/(public)/account/campaign/[email]/[id]/page.tsx
@@ -1,5 +1,5 @@
 // pages/redirect.js
-"use client"; // This directive marks the file as a client component
+//"use client"; // This directive marks the file as a client component
 import { useState, useEffect } from "react";
 import { signIn } from "next-auth/react";
 //import { useEffect } from 'react';
@@ -9,42 +9,51 @@ const RedirectPage = async({
 }: {
   params: { email: string, id:string };
 }) => {
-  const decodedEmail = atob(params.email);
-  const initialValues = {
-    userEmail: decodedEmail,
-    userPassword: "",
-    domain: ""
-  };
-  console.log(params.email);
-  console.log(params.id);
+  // const decodedEmail = atob(params.email);
+  // const initialValues = {
+  //   userEmail: decodedEmail,
+  //   userPassword: "",
+  //   domain: ""
+  // };
+  // console.log(params.email);
+  // console.log(params.id);
  
-  console.log(decodedEmail);
-  const [data, setData] = useState(initialValues);
-  const response = await fetch("/api/login", {
-    method: "POST",
-    body: JSON.stringify(data)
-  });
+  // console.log(decodedEmail);
+  // const [data, setData] = useState(initialValues);
+  // const response = await fetch("/api/login", {
+  //   method: "POST",
+  //   body: JSON.stringify(data)
+  // });
 
-  if (response.ok) {
+  // if (response.ok) {
    
-    console.log(response);
-    await signIn("credentials", {
-      redirect: false,
-      email: data.userEmail,
-      password: data.userPassword,
-      domain: data.domain
-    });
-    window.location.replace("/dashboard");
-  } else {
-    console.log("login error");
+  //   console.log(response);
+  //   await signIn("credentials", {
+  //     redirect: false,
+  //     email: data.userEmail,
+  //     password: data.userPassword,
+  //     domain: data.domain
+  //   });
+  //   window.location.replace("/dashboard");
+  // } else {
+  //   console.log("login error");
    
-  }
+  // }
+
  /* useEffect(() => {
     // Redirect to the homepage
     window.location.href = '/';
   }, []);*/
 
- 
+  return (
+    <>
+      <br></br>  
+      <br></br>
+      <h1>email {atob(decodeURIComponent(params.email))}</h1>
+      <h1>id {params.id}</h1>
+    </>
+  )
 };
+
 
 export default RedirectPage;
