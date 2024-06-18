5190f93e9c1218a1aaf885ff8d089451e045be51
jaysoncano <cano.jayson10@gmail.com>
2024-06-18 13:34:51 +0800

    Fix: UI autologin

--- app/(public)/account/campaign/[email]/[id]/components/login.tsx
+++ app/(public)/account/campaign/[email]/[id]/components/login.tsx
@@ -3,29 +3,37 @@ import { ReloadIcon, CaretSortIcon } from "@radix-ui/react-icons";
 import { useEffect } from "react";
 import { signIn } from "next-auth/react";
 interface Props {
-    campaign_id: string;
-    email: string;
-  }
+  campaign_id: string;
+  email: string;
+}
 const Login = ({ campaign_id, email }: Props) => {
-    useEffect(() => {
-        async function Signin() {
-            await signIn("credentials", {
-                redirect: false,
-                email: email,
-                campaign_id: campaign_id
-              });
-              window.location.replace("/dashboard");
-          }
-        Signin()
-
-        
-        // eslint-disable-next-line
-      },[]);
-    return (
-      <>
-        <br></br>
-        <ReloadIcon className="h-4 w-4 animate-spin mr-2" /> Redirecting 
-      </>
-      )
+  useEffect(() => {
+    async function Signin() {
+      await signIn("credentials", {
+        redirect: false,
+        email: email,
+        campaign_id: campaign_id
+      });
+      window.location.replace("/dashboard");
     }
+    Signin();
+
+    // eslint-disable-next-line
+  }, []);
+  return (
+    <>
+      <main>
+        <section className="min-h-screen flex flex-col justify-center items-center bg-[#fafafa]">
+          <div className="w-full max-w-sm">
+            <div className="flex flex-col items-center justify-center">
+              <div className="text-center flex text-2xl font-semi text-gray-800/60 items-center justify-center">
+                <ReloadIcon className="h-4 w-4 animate-spin mr-2" /> Please wait ...
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+};
 export default Login;
